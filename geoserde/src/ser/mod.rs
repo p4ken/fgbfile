03db825ba9473e57c869f311a92e02ce4813049d
p4ken <commit@p4ken.skr.jp>
2023-12-17 15:52:32 +0900

    remove geoserde

--- geoserde/src/ser/mod.rs
+++
@@ -1,9 +0,0 @@
-mod err;
-mod feat;
-mod geom;
-mod prop;
-
-pub use err::SerializeError;
-pub use feat::FeatureSerializer;
-pub use geom::{GeometrySerializer, GeometrySink};
-pub use prop::{PropertySerializer, PropertySink};
